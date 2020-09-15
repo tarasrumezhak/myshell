0384f7aacdca5bf09c666f515d4cf2597cb0678b
taras.rumezhak <taras.rumezhak@kindgeek.com>
2020-09-15 11:41:34 +0300

    starting code

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,98 @@
 #include <iostream>
 #include <boost/program_options.hpp>
-
+#include <sys/wait.h>
 #include "operations/operations.hpp"
 
+#define MAXDIR 4096
+
+
+char* get_cwd() {
+//    char buf[4096];
+//    getcwd(buf, 4096);
+//    return buf;
+
+    char dir[MAXDIR];
+    char *result = getcwd(dir, MAXDIR);
+    //printf("Current directory is %s", dir);
+    return result;
+}
+
+bool replace(std::string& str, const std::string& from, const std::string& to) {
+    size_t start_pos = str.find(from);
+    if(start_pos == std::string::npos)
+        return false;
+    str.replace(start_pos, from.length(), to);
+    return true;
+}
+
+//std::string preprocess_line(std:: string line) {
+//
+//}
+
+
 int main(int argc, char **argv) {
-    int variable_a, variable_b;
+//    std::cout << get_cwd() << '$';
+    while(true) {
+        std::cout << get_cwd() << '$';
+        std::string line;
+        std::getline (std::cin,line);
+        std::cout << "You entered: " << line << "\n";
 
-    namespace po = boost::program_options;
+        std::istringstream ss(line);
+        std::string token;
 
-    po::options_description visible("Supported options");
-    visible.add_options()
-            ("help,h", "Print this help message.");
+        std::vector<std::string> args;
 
-    po::options_description hidden("Hidden options");
-    hidden.add_options()
-            ("a", po::value<int>(&variable_a)->default_value(0), "Variable A.")
-            ("b", po::value<int>(&variable_b)->default_value(0), "Variable B.");
+        while(std::getline(ss, token, ' ')) {
+            std::cout << token << '\n';
+            args.push_back(token);
+        }
 
-    po::positional_options_description p;
-    p.add("a", 1);
-    p.add("b", 1);
+        std::string victim_name(args[0]);
 
-    po::options_description all("All options");
-    all.add(visible).add(hidden);
+        auto path_ptr = getenv("PATH");
+        std::string path_var;
+        if(path_ptr != nullptr)
+            path_var = path_ptr;
+        path_var += ":.";
+        setenv("PATH", path_var.c_str(), 1);
 
-    po::variables_map vm;
-    po::store(po::command_line_parser(argc, argv).options(all).positional(p).run(), vm);
-    po::notify(vm);
+        std::vector<const char*> arg_for_c;
+        for(auto s: args)
+            arg_for_c.push_back(s.c_str());
+        arg_for_c.push_back(nullptr);
 
-    if (vm.count("help")) {
-        std::cout << "Usage:\n  add [a] [b]\n" << visible << std::endl;
-        return EXIT_SUCCESS;
+        pid_t parent = getpid();
+        pid_t pid = fork();
+
+        if (pid == -1)
+        {
+            std::cerr << "Failed to fork()" << std::endl;
+            exit(EXIT_FAILURE);
+        }
+        else if (pid > 0)
+        {
+            // We are parent process
+//            std::cout << "Parent: Hello from parent" << std::endl;
+//            std::cout << "Parent: Parent PID: " << parent << ", child PID: " << pid << std::endl;
+            int status;
+            waitpid(pid, &status, 0);
+//            std::cout << "Parent: child stopped, exit code: " << status << std::endl;
+        }
+        else
+        {
+            // We are the child
+//            execve(victim_name.c_str(), args, environ);
+            std::cout << "\t running " << line << std::endl;
+
+//            std::string to_exec = "usr/bin/" + victim_name;
+
+            execvp(victim_name.c_str(), const_cast<char* const*>(arg_for_c.data()));
+//            std::cerr << "Parent: Failed to execute " << victim_name << " \n\tCode: " << errno << std::endl;
+            exit(EXIT_FAILURE);   // exec never returns
+        }
+
     }
+    return 0;
 
-    int result = operations::add(variable_a, variable_b);
-    std::cout << result << std::endl;
-    return EXIT_SUCCESS;
 }
---
+++ src/victim_ls.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <unistd.h>
+#include <sys/types.h>
+
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::string;
+extern char **environ;
+
+
+int main() {
+    pid_t parent = getppid();
+    pid_t pid = getpid();
+//    std::cout << "Child: Hello from child!" << std::endl;
+//    std::cout << "Child: Parent PID: " << parent << ", child PID: " << pid << endl;
+//    std::cout << "And now running ls: " << endl;
+    system("ls");
+    return 0;
+}
